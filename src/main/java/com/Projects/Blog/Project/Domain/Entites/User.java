14a84e929bcf8e3cbe6bcc3dd3972d015251ba86
shady ayman <shadyayman450@gmail.com>
2025-11-05 03:41:17 +0200

    add user&category entites

--- src/main/java/com/Projects/Blog/Project/Domain/Entites/User.java
+++ src/main/java/com/Projects/Blog/Project/Domain/Entites/User.java
@@ -1,4 +1,51 @@
 package com.Projects.Blog.Project.Domain.Entites;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false , unique = true)
+    private String Email;
+
+    @Column(nullable = false)
+    private String Password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(Email, user.Email) && Objects.equals(Password, user.Password) && Objects.equals(name, user.name) && Objects.equals(createdAt, user.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, Email, Password, name, createdAt);
+    }
+
+    @PrePersist
+    protected  void onCreate() {
+        this.createdAt=LocalDateTime.now();
+    }
 }
