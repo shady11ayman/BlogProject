34cd0d6efa6d7ebacd0b476c17a05bd065ae00ce
shady ayman <shadyayman450@gmail.com>
2025-11-19 18:54:43 +0200

    added repos,services,dto and endpoint

--- src/main/java/com/Projects/Blog/Project/Repositories/CategoryRepository.java
+++ src/main/java/com/Projects/Blog/Project/Repositories/CategoryRepository.java
@@ -1,4 +1,16 @@
 package com.Projects.Blog.Project.Repositories;
 
-public interface CategoryRepository {
+import com.Projects.Blog.Project.Domain.Entites.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+    @Query("SELECT c FROM Category c LEFT JOIN FETCH c.posts")
+    List<Category> findAllWithPostCount();
 }
